ece76fbdc4032c562412fb1b497eb3e248f7aeac
yarkin <a.yarkin@outlook.com>
2022-04-30 23:20:46 +0300

    add function project

---
+++ TestProject_x=f(a)/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу вычисления функции: x = f(a)
+
+Console.Write ("Enter the number: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+int x = 4 * a * a + 17 * a + 8;
+Console.WriteLine(x);
