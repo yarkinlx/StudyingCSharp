6e8f621bbfe7175f056e92ecaf82bde26145875e
yarkin <a.yarkin@outlook.com>
2022-04-30 22:39:48 +0300

    Add SquaringCheck

---
+++ TestProject_SquaringCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+
+Console.Write ("Enter the first number: ");
+string s_a = Console.ReadLine();
+Console.Write ("Enter the second number: ");
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if(a * a == b)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
