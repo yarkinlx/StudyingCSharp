9673d7de5894a11873eb8d2b66126a4622d83202
yarkin <a.yarkin@outlook.com>
2022-04-30 18:42:15 +0300

    add IfElse project

---
+++ TestProject_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Hi Alex, you're awesome!");
+}
+else
+{
+    Console.Write("Hi ");
+    Console.WriteLine(username);
+}
