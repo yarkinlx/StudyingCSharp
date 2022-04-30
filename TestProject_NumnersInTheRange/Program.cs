377cc642819d593015deb48dd1d059a0a8a92756
yarkin <a.yarkin@outlook.com>
2022-05-01 00:18:47 +0300

    add numers in the range project

---
+++ TestProject_NumnersInTheRange/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число(N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Enter the number: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+int b = -a;
+
+
+while (b <= a)
+{
+    Console.Write($"{b}, ");
+    b = b + 1;
+}
