71fc2860f8b10aa118e5100d200a7322cae1ac41
yarkin <a.yarkin@outlook.com>
2022-05-22 07:12:44 +0300

    add new exercise

---
+++ TableOfSquares/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void Squares(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"{n} -> ");
+
+    while(index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 2);
+        Console.Write($"{nums[index]}, ");
+        index++;
+    }
+    Console.WriteLine();
+}
+
+Squares(2);
+Squares(5);
+Squares(11);
