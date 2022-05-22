f6ee2ae9c2aaf76f292151eae3d9a15e32375583
yarkin <a.yarkin@outlook.com>
2022-05-22 07:34:23 +0300

    Add first task

---
+++ Homework_3/PalindromeCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void PalindromeCheck()
+{
+    Console.WriteLine("Enter a five digit number: ");
+    string number = Console.ReadLine();
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Yes, your number is palindrome");
+    }
+    else
+    {
+        Console.WriteLine("No, your number is not palindrome");
+    }
+}
+
+PalindromeCheck();
+PalindromeCheck();
+PalindromeCheck();
