ea60bb1e34f277eb450a6727e596e4f494a94e6e
yarkin <a.yarkin@outlook.com>
2022-05-22 08:24:57 +0300

    add task 3

---
+++ Homework_3/ThirdDegreeTable/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void ThirdDegree(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"{n} -> ");
+
+    while(index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 3);
+        Console.Write($"{nums[index]}, ");
+        index++;
+    }
+    Console.WriteLine();
+ 
+}
+
+ThirdDegree(5);
+ThirdDegree(7);
+ThirdDegree(9);
+
+
