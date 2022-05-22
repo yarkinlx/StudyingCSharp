967f35b3dd082d5cf027e940978332aea062c2f4
yarkin <a.yarkin@outlook.com>
2022-05-22 06:30:23 +0300

    add quarters

---
+++ quarters/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая по заданному номеру четверти,
+//показывает диапазон возможных координат точек в этой четверти (x и y)
+
+
+void QuarterCheck()
+{
+    Console.Write("Enter the quarter number:");
+    string s_number = Console.ReadLine();
+    int quarter = int.Parse(s_number);
+    if(quarter == 1)
+    {
+        Console.WriteLine("X(0;+∞),Y(0;+∞)");
+    }
+    if(quarter == 2)
+    {
+        Console.WriteLine("X(-∞;0),Y(0;+∞)");
+    }
+    if(quarter == 3)
+    {
+        Console.WriteLine("X(-∞;0),Y(-∞;0)");
+    }
+    if(quarter == 4)
+    {
+        Console.WriteLine("X(0;+∞),Y(-∞;0)");
+    }
+    else
+    {
+        Console.WriteLine("enter the valid number (from 1 to 4)");
+    }
+}
+
+
+
+QuarterCheck();
+QuarterCheck();
+QuarterCheck();
+QuarterCheck();
