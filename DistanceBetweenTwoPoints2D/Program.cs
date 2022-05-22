3961a9c2648274730709948f5b0419225a37c81a
yarkin <a.yarkin@outlook.com>
2022-05-22 07:12:22 +0300

    add new exercise

---
+++ DistanceBetweenTwoPoints2D/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)), 4);
+}
+
+Console.WriteLine(Distance(3, 6, 2, 1));
+Console.WriteLine(Distance(7, -5, 1, -1));
