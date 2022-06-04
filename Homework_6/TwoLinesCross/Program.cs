80b4ddf2477a598801c5615cdc29b92fb6406129
yarkin <a.yarkin@outlook.com>
2022-06-04 09:41:26 +0300

    add task

---
+++ Homework_6/TwoLinesCross/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.Write("There is no crossing point");
+}
+
+else
+{
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Crossing point is ({x};{y})");
+}
