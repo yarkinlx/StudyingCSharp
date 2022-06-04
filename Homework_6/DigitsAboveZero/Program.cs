b48040a1d779bafc30f82cf03d931ce8f2d85636
yarkin <a.yarkin@outlook.com>
2022-06-04 09:19:19 +0300

    add task

---
+++ Homework_6/DigitsAboveZero/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write($"How much numbers will be put: ");
+int q = int.Parse(Console.ReadLine());
+int[] arrayNumbers = new int[q];
+
+
+
+void HowMuch(int q)
+{
+    for (int i = 0; i < q; i++)
+    {
+        Console.Write($"Enter the next number: ");
+        arrayNumbers[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+int PositiveCheck(int[] arrayNum)
+{
+    int count = 0;
+    for (int i = 0; i < arrayNum.Length; i++)
+    {
+        if (arrayNum[i] > 0)
+        {
+            count += 1;
+        } 
+    }
+    return count;
+}
+
+
+
+HowMuch(q);
+
+Console.WriteLine($"The number of digits above zero is {PositiveCheck(arrayNumbers)}");
